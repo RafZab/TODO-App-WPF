09679e943f061e1dc622fb22e2b8f8c6bbb65932
Sylwia Mościcka <sysazeszkoly@gmail.com>
2022-05-19 00:12:23 +0200

    change header after click

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -19,6 +19,40 @@ namespace TodoApp
 
         public ObservableCollection<ToDoTask> Tasks { get; set; } = new ObservableCollection<ToDoTask>();
 
+        void onClick_wazne(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Ważne";
+        }
+
+        void onClick_zaplanowane(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Zaplanowane";
+        }
+
+        void onClick_do_mnie(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Przydzielone do mnie";
+        }
+
+        void onClick_mojdzien(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Mój dzień";
+        }
+
+        void onClick_zadania(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Zadania";
+        }
+
+        void onClick_wprowadzenie(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Wprowadzenie";
+        }
+
+        void onClick_artykuly(object sender, RoutedEventArgs e)
+        {
+            headline.Text = "Artykuły spożywcze";
+        }
 
 
     }
