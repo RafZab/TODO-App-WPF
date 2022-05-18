87d40a0d518508eb56ab38a08fc467dc2c8b1044
Szymon Kozłowski <40643840+ymorii@users.noreply.github.com>
2022-05-18 19:28:28 +0200

    Task lists (#4)

---
+++ TodoApp/Models/Task.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApp.Models
+{
+    public class ToDoTask
+    {
+        public string Name { get; set; }
+
+        public bool IsDone { get; set; }
+    }
+}
