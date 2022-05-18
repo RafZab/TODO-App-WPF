85368e50bbe206314a3f90f0a258d881ec05b557
Sylwia Mościcka <sysazeszkoly@gmail.com>
2022-05-19 00:22:15 +0200

    change color header

--- TodoApp/MainWindow.xaml.cs
+++ TodoApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Media;
 using TodoApp.Models;
 
 namespace TodoApp
@@ -22,36 +23,58 @@ namespace TodoApp
         void onClick_wazne(object sender, RoutedEventArgs e)
         {
             headline.Text = "Ważne";
+            headline.Foreground = Brushes.Red;
+            text.Foreground = Brushes.Red;
+            kind.Foreground = Brushes.Red;
         }
 
         void onClick_zaplanowane(object sender, RoutedEventArgs e)
         {
             headline.Text = "Zaplanowane";
+            headline.Foreground = Brushes.Green;
+            text.Foreground = Brushes.Green;
+            kind.Foreground = Brushes.Green;
         }
 
         void onClick_do_mnie(object sender, RoutedEventArgs e)
         {
             headline.Text = "Przydzielone do mnie";
+            headline.Foreground = Brushes.Green;
+            text.Foreground = Brushes.Green;
+            kind.Foreground = Brushes.Green;
         }
 
         void onClick_mojdzien(object sender, RoutedEventArgs e)
         {
             headline.Text = "Mój dzień";
+            headline.Foreground = Brushes.Purple;
+            text.Foreground = Brushes.Purple;
+            kind.Foreground = Brushes.Purple;
         }
 
         void onClick_zadania(object sender, RoutedEventArgs e)
         {
             headline.Text = "Zadania";
+            headline.Foreground = Brushes.Red;
+            text.Foreground = Brushes.Red;
+            kind.Foreground = Brushes.Red;
         }
 
         void onClick_wprowadzenie(object sender, RoutedEventArgs e)
         {
             headline.Text = "Wprowadzenie";
+            headline.Foreground = Brushes.Yellow;
+            text.Foreground = Brushes.Yellow;
+            kind.Foreground = Brushes.Yellow;
+
         }
 
         void onClick_artykuly(object sender, RoutedEventArgs e)
         {
             headline.Text = "Artykuły spożywcze";
+            headline.Foreground = Brushes.Gray;
+            text.Foreground = Brushes.Gray;
+            kind.Foreground = Brushes.Gray;
         }
 
 
