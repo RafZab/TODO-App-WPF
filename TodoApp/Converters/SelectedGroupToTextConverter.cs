ad8b63b21b9024ab0fc21a0b87bad3dec06fbade
Szymon Kozłowski <kierowcataczkipl@gmail.com>
2022-05-19 17:26:54 +0200

    Fuck that

---
+++ TodoApp/Converters/SelectedGroupToTextConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media.Animation;
+using TodoApp.Models;
+
+namespace TodoApp.Converters
+{
+    public class SelectedGroupToTextConverter : IValueConverter
+    {
+        public string FallBackValue { get; set; } = "New Group";
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not TaskGroup taskGroup)
+            {
+                return FallBackValue;
+            }
+
+            return taskGroup.Name;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TodoApp/PropertyControl.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MaterialDesignThemes.Wpf;
+
+namespace TodoApp
+{
+    /// <summary>
+    /// Interaction logic for PropertyControl.xaml
+    /// </summary>
+    public partial class PropertyControl : UserControl
+    {
+        public static readonly DependencyProperty IconHeightProperty =
+            DependencyProperty.Register("IconHeigh", typeof(double), typeof(PropertyControl), new PropertyMetadata(null));
+        
+        public static readonly DependencyProperty IconWidthProperty =
+            DependencyProperty.Register("IconWidth", typeof(double), typeof(PropertyControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IconKindProperty =
+            DependencyProperty.Register("IconKind", typeof(PackIconKind), typeof(PropertyControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty IconBrushProperty =
+            DependencyProperty.Register("IconBrush", typeof(Brush), typeof(PropertyControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(PropertyControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty TextFontSizeProperty =
+            DependencyProperty.Register("TextFontSize", typeof(double), typeof(PropertyControl), new PropertyMetadata(null));
+
+        public static readonly DependencyProperty TextBrushProperty =
+            DependencyProperty.Register("TextBrush", typeof(Brush), typeof(PropertyControl), new PropertyMetadata(null));
+
+        public double IconHeight
+        {
+            get => (double)GetValue(IconHeightProperty);
+            set => SetValue(IconHeightProperty, value);
+        }
+
+        public double IconWidth
+        {
+            get => (double)GetValue(IconWidthProperty);
+            set => SetValue(IconWidthProperty, value);
+        }
+
+        public PackIconKind IconKind
+        {
+            get => (PackIconKind)GetValue(IconKindProperty);
+            set => SetValue(IconKindProperty, value);
+        }
+
+        public Brush IconBrush
+        {
+            get => (Brush)GetValue(IconBrushProperty);
+            set => SetValue(IconBrushProperty, value);
+        }
+
+        public string Text
+        {
+            get => (string)GetValue(TextProperty);
+            set => SetValue(TextProperty, value);
+        }
+
+        public double TextFontSize
+        {
+            get => (double)GetValue(TextFontSizeProperty);
+            set => SetValue(TextFontSizeProperty, value);
+        }
+
+        public Brush TextBrush
+        {
+            get => (Brush)GetValue(TextBrushProperty);
+            set => SetValue(TextBrushProperty, value);
+        }
+
+        public SolidColorBrush FallbackBrush => new(Colors.Gray);
+
+        public PropertyControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
