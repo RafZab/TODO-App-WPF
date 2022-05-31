dd8750fc0a1b4a37627adde6370ad92e4f5ddd31
Szymon Kozlowski <kierowcataczkipl@gmail.com>
2022-05-31 22:15:21 +0200

    p1

---
+++ TodoApp/Converters/SelectedItemToSubtaskVisiblityConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+using TodoApp.Models;
+
+namespace TodoApp.Converters
+{
+    public class SelectedItemToSubtaskVisiblityConverter : IValueConverter
+    {
+        public bool Inverted { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is not ToDoTask toDoTask)
+            {
+                return value;
+            }
+
+            if (toDoTask.SubTasks is null)
+            {
+                return Inverted ? Visibility.Visible : Visibility.Hidden;
+            }
+
+            if (Inverted)
+            {
+                return toDoTask.SubTasks.Count > 0 ? Visibility.Hidden : Visibility.Visible;
+            }
+
+            return toDoTask.SubTasks.Count > 0 ? Visibility.Visible : Visibility.Hidden;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- TodoApp/Models/ToDoTask.cs
+++ TodoApp/Models/ToDoTask.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,15 @@ using System.Threading.Tasks;
 namespace TodoApp.Models
 {
     public class ToDoTask
+    {
+        public string Name { get; set; }
+
+        public bool IsDone { get; set; }
+
+        public ObservableCollection<ToDoSubTask> SubTasks { get; set; } 
+    }
+
+    public class ToDoSubTask
     {
         public string Name { get; set; }
 
